0926f0cf1df1bf2fbcf1f5b7610d169ba42c6add
joesRepos <joemitchell1717@gmail.com>
2026-01-15 17:28:22 +0000

    Construct the board.

--- BattleShips.java
+++ BattleShips.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class BattleShips {
     public static void main(String[] args) {
+        play();
+    }
+
+    public static String[][] createBoard(int rows, int cols) {
+        String[][] board = new String[rows * 2][(cols * 2) + 1];
+
+        for (int i = 0; i < rows * 2; i++) {
+            for (int j = 0; j < (cols * 2) + 1; j++) {
+                if (j == cols) {
+                    board[i][j] = "|";
+                }
+                else {
+                    board[i][j] = "O";
+                }
+            }
+        }
+        return board;
+    }
+
+    public static void play() {
         System.out.println("Welcome to Battle Ships!");
+        Scanner scanner = new Scanner(System.in);
+        int rowsSize = 10;
+        int columnsSize = 10;
+        String[][] gameBoard = createBoard(rowsSize, columnsSize);
+
     }
 }
