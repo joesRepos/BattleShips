1ead9815f3f67ab9ae8568790094a7284e5006c4
joesRepos <joemitchell1717@gmail.com>
2026-01-16 20:42:04 +0000

    Print the board.

--- BattleShips.java
+++ BattleShips.java
@@ -6,7 +6,7 @@ public class BattleShips {
     }
 
     public static String[][] createBoard(int rows, int cols) {
-        String[][] board = new String[rows * 2][(cols * 2) + 1];
+        String[][] board = new String[rows * 2][(cols * 2) + 2];
 
         for (int i = 0; i < rows * 2; i++) {
             for (int j = 0; j < (cols * 2) + 1; j++) {
@@ -17,8 +17,17 @@ public class BattleShips {
                     board[i][j] = "O";
                 }
             }
+            board[i][(cols * 2) + 1] = "\n";
         }
         return board;
+    }
+
+    public static void displayBoard(String[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+        }
     }
 
     public static void play() {
@@ -27,6 +36,7 @@ public class BattleShips {
         int rowsSize = 10;
         int columnsSize = 10;
         String[][] gameBoard = createBoard(rowsSize, columnsSize);
+        displayBoard(gameBoard);
 
     }
 }
